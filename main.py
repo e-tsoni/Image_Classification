2734d88a9d39a22d144b5d829c98f30ec29bb9a6
e-tsoni <etsoni.cs@gmail.com>
2024-02-16 16:02:58 +0200

    initial commit master

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
